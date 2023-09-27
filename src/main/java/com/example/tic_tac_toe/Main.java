f08b292f162182231777d108e99e62b6c35e8a20
Viktor <fokus11548@mail.ru>
2023-09-27 15:59:39 +0400

    Add Map and SettingsWindow

--- src/main/java/com/example/tic_tac_toe/Main.java
+++ src/main/java/com/example/tic_tac_toe/Main.java
@@ -2,6 +2,7 @@ package com.example.tic_tac_toe;
 
 public class Main {
     public static void main(String[] args) {
-
+        new GameWindow();
+        System.out.println("Method main() is over");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.tic_tac_toe {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.tic_tac_toe to javafx.fxml;
